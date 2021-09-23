26fd4e0a22420a94adc4e2890548d39b73ecd67b
Paul <paul@enclumeanimation.com>
2021-09-23 10:29:58 +0200

    version change

--- __init__.py
+++ __init__.py
@@ -18,7 +18,7 @@ bl_info = {
     "author" : "Paul",
     "description" : "",
     "blender" : (2, 91, 0),
-    "version" : (1, 10, 2),
+    "version" : (1, 10, 4),
     "location" : "View3D",
     "warning" : "",
     "category" : "",
